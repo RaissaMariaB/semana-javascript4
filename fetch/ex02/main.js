87aea134f0ad4a5e72666a672c72a7d31f8b2118
RaissaMariaB <raissamaria.sb@gmail.com>
2019-11-08 11:55:35 -0300

    adiciona exercicio de de cep correio

--- fetch/ex02/main.js
+++ fetch/ex02/main.js
@@ -1,14 +1,34 @@
 const cepInput = document.getElementById("cep")
 const logradouroInput = document.getElementById("logradouro")
+const complementoInput = document.getElementById("complemento")
+const bairroInput = document.getElementById("bairro")
+const localidadeInput = document.getElementById("localidade")
+const ufInput = document.getElementById("uf")
+
 //... continuem a pegar os inputs do index.html para preencher depois com o valor da api
 
+ renderizaInput = json => {
+    
+    logradouroInput.value = json.logradouro
+    complementoInput.value = json.complemento
+    bairroInput.value = json.bairro
+    localidadeInput.value = json.localidade
+    ufInput.value = json.uf
+     
+}
+ 
+
 cepInput.addEventListener("blur", ()=>{
-    if(cepInput.input){
+    if(cepInput.value){
         fetch(`https://viacep.com.br/ws/${cepInput.value}/json`).then(response => {
             return response.json()
-        }).then(json => {
+        }).then(json => {   
+            renderizaInput(json)
+            
             ///façam aparecer os valores nos inputs
-
+            
         })
     }
 })
+
+
